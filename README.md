5411c005ccbf2e0482739bebd2560fa1f973061e
Kelvin Samuel <kelvin@kks110.com>
2019-03-19 12:22:04 +0000

    Convert digits to roman numerals - complete

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Roman Numerals Converter
 A little app to convert digits to Roman Numerals.
 
-# Upcoming Features
+# Complete Features
 * Convert digits to Roman Numerals
+
+# Upcoming Features
+
 * Convert Roman Numerals to digits
 * Maths with Roman Numerals
--- converter.rb
+++ converter.rb
@@ -1,20 +1,28 @@
 
 class Converter
+  # The Roman numerals.
   @@units = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
   @@tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
   @@hundreds = ["C", "CC", "CCC", "DC", "D", "DC", "DCC", "DCCC", "CM"]
-  @@thousands = ["M"]
+  @@thousands = ["M", "MM", "MMM"]
 
+  # Gets the number.
   def get_number
     puts "Please enter a number between 1 and 3999: "
     user_input = gets.chomp
-    if valid_input?(user_input)
-      puts "Valid input"
-    else
-      puts "Invalid input"
+
+    # Checks its valid.
+    while !valid_input?(user_input)
+      puts "Invalid input, please enter a number between 1 and 3999: "
+      user_input = gets.chomp
     end
+    puts ""
+    # Converts it.
+    convert(user_input)
+
   end
 
+  # Checks if input is a number, then if its between 1 and 3999.
   def valid_input?(input)
     if input =~ /\A\d+\Z/
       if input.to_i > 0 and input.to_i < 4000
@@ -27,7 +35,35 @@ class Converter
     end
   end
 
+  # Takes the input. Makes it an array.
+  # Does the conversion, outputs what that number is and adds it to the numerals array.
+  # Then out puts the final number.
+  def convert(input)
+    numerals_string = []
+    input = input.split("")
 
+    puts "   #{input.last} = #{@@units[input.last.to_i - 1]}"
+    numerals_string.unshift( @@units[input.pop.to_i - 1] )
+
+    if !input.empty?
+      puts "  #{input.last}0 = #{@@tens[input.last.to_i - 1]}"
+      numerals_string.unshift( @@tens[input.pop.to_i - 1] )
+    end
+
+    if !input.empty?
+      puts " #{input.last}00 = #{@@hundreds[input.last.to_i - 1]}"
+      numerals_string.unshift( @@hundreds[input.pop.to_i - 1] )
+    end
+
+    if !input.empty?
+      puts "#{input.last}000 = #{@@thousands[input.last.to_i - 1]}"
+      numerals_string.unshift( @@thousands[input.pop.to_i - 1] )
+    end
+
+    puts ""
+    puts "Your number converted to Roman Numerals is: "
+    puts "#{numerals_string.join}"
+  end
 
 
 end
