41d078771004d7bdf297de8e8f2eebc19a866adb
Kelvin Samuel <kelvin@kks110.com>
2019-05-03 14:20:16 +0100

    code refactor, bug found with 0's

--- converter.rb
+++ converter.rb
@@ -1,67 +1,70 @@
 
 class Converter
   # The Roman numerals.
-  @@units = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
-  @@tens = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
-  @@hundreds = ["C", "CC", "CCC", "DC", "D", "DC", "DCC", "DCCC", "CM"]
-  @@thousands = ["M", "MM", "MMM"]
+
+  def initialize
+    @units = %w[I II III IV V VI VII VIII IX]
+    @tens = %w[X XX XXX XL L LX LXX LXXX XC]
+    @hundreds = %w[C CC CCC DC D DC DCC DCCC CM]
+    @thousands = %w[M MM MMM]
+  end
 
   # Gets the number.
-  def get_number
-    puts "Please enter a number between 1 and 3999: "
+  def enter_number
+    puts 'Please enter a number between 1 and 3999: '
     user_input = gets.chomp
 
     # Checks its valid.
-    while !valid_input?(user_input)
-      puts "Invalid input, please enter a number between 1 and 3999: "
+    until valid_input?(user_input)
+      puts 'Invalid input, please enter a number between 1 and 3999: '
       user_input = gets.chomp
     end
-    puts ""
+    puts ''
     # Converts it.
-    convert(user_input)
-
+    roman_convert(user_input)
   end
 
   # Checks if input is a number, then if its between 1 and 3999.
   def valid_input?(input)
     if input =~ /\A\d+\Z/
-      if input.to_i > 0 and input.to_i < 4000
-        return true
+      if input.to_i > 0 && input.to_i < 4000
+        true
       else
-        return false
+        false
       end
     else
-      return false
+      false
     end
   end
 
   # Takes the input. Makes it an array.
-  # Does the conversion, outputs what that number is and adds it to the numerals array.
+  # Does the conversion, outputs what that number is
+  # and adds it to the numerals array.
   # Then out puts the final number.
-  def convert(input)
+  def roman_convert(input)
     numerals_string = []
-    input = input.split("")
+    input = input.split('')
 
-    puts "   #{input.last} = #{@@units[input.last.to_i - 1]}"
-    numerals_string.unshift( @@units[input.pop.to_i - 1] )
+    puts "   #{input.last} = #{@units[input.last.to_i - 1]}"
+    numerals_string.unshift( @units[input.pop.to_i - 1])
 
-    if !input.empty?
-      puts "  #{input.last}0 = #{@@tens[input.last.to_i - 1]}"
-      numerals_string.unshift( @@tens[input.pop.to_i - 1] )
+    unless input.empty?
+      puts "  #{input.last}0 = #{@tens[input.last.to_i - 1]}"
+      numerals_string.unshift( @tens[input.pop.to_i - 1])
     end
 
-    if !input.empty?
-      puts " #{input.last}00 = #{@@hundreds[input.last.to_i - 1]}"
-      numerals_string.unshift( @@hundreds[input.pop.to_i - 1] )
+    unless input.empty?
+      puts " #{input.last}00 = #{@hundreds[input.last.to_i - 1]}"
+      numerals_string.unshift( @hundreds[input.pop.to_i - 1])
     end
 
-    if !input.empty?
-      puts "#{input.last}000 = #{@@thousands[input.last.to_i - 1]}"
-      numerals_string.unshift( @@thousands[input.pop.to_i - 1] )
+    unless input.empty?
+      puts "#{input.last}000 = #{@thousands[input.last.to_i - 1]}"
+      numerals_string.unshift( @thousands[input.pop.to_i - 1])
     end
 
-    puts ""
-    puts "Your number converted to Roman Numerals is: "
+    puts ''
+    puts 'Your number converted to Roman Numerals is: '
     puts "#{numerals_string.join}"
   end
 
--- init.rb
+++ init.rb
@@ -1,15 +1,15 @@
 require './converter'
 
-puts "You can write numbers between 1 and 3999 using Roman Numerals."
-puts "I = 1"
-puts "V = 5"
-puts "X = 10"
-puts "L = 50"
-puts "C = 100"
-puts "D = 500"
-puts "M = 1000"
-puts ""
+puts 'You can write numbers between 1 and 3999 using Roman Numerals.'
+puts 'I = 1'
+puts 'V = 5'
+puts 'X = 10'
+puts 'L = 50'
+puts 'C = 100'
+puts 'D = 500'
+puts 'M = 1000'
+puts ''
 
 converter = Converter.new
 
-converter.get_number
+converter.enter_number
